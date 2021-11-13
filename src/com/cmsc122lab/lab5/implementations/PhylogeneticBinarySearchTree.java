7533e7286170d370c3cece02d75ee654c69d6e22
nj <nacastillo2@up.edu.ph>
2021-11-13 17:01:45 +0800

    insert search delete works

--- src/com/cmsc122lab/lab5/implementations/PhylogeneticBinarySearchTree.java
+++ src/com/cmsc122lab/lab5/implementations/PhylogeneticBinarySearchTree.java
@@ -47,58 +47,84 @@ public class PhylogeneticBinarySearchTree implements PhylogeneticBSTADT {
         System.out.println(species.getName() + ": " + species.getLineage());
     }
 
-    @Override
+    @Override // a recursive function to insert a new key in BST
     public PhylogeneticBSTNode insertSpecies(PhylogeneticBSTNode root, Species species) {
-        // implement tree insert here and modify return value
+        //if tree is empty, return a new node
         if(root == null) {
             root = new PhylogeneticBSTNode(species);
             return root;
         }
-        else {
-            PhylogeneticBSTNode current = root;
 
-
-            while(true){
-                if (root.getSpecieData() == current.getSpecieData()){
-                    return null; //no duplicates
-                }
-                else if(root.getSpecieData().getLineage() < current.getSpecieData().getLineage() && current.getLeftSpecieData() == null){
-                    current.getLeftSpecieData().setLeftSpecieData(new PhylogeneticBSTNode(species)); //value inserted on the left
-                    break;
-                }
-                else if (root.getSpecieData().getLineage() < current.getSpecieData().getLineage()){
-                    current = current.getLeftSpecieData();
-                }
-                else if (root.getSpecieData().getLineage() > current.getSpecieData().getLineage() && current.getRightSpecieData() == null){
-                    current.getRightSpecieData().setRightSpecieData(new PhylogeneticBSTNode(species));
-                }
-                else{
-                    current = current.getRightSpecieData();
-                }
-
-            }
+        //otherwise, recur down the tree
+        if(species.getLineage() < root.getSpecieData().getLineage()){
+            root.setLeftSpecieData(insertSpecies(root.getLeftSpecieData(), species));
         }
+        else if (species.getLineage() > root.getSpecieData().getLineage()){
+            root.setRightSpecieData(insertSpecies(root.getRightSpecieData(), species));
+        }
+
+        //return the node pointer
         return root;
     }
 
-    @Override
+    @Override //a recursive function to delete an existing species in BST
     public PhylogeneticBSTNode deleteSpecies(PhylogeneticBSTNode root, Species species) {
-        //implement tree delete here and modify return value
+        //base case: if the tree is empty
+        if(root == null)
+            return root;
+
+        //otherwise, recur down the tree
+        if (species.getLineage() < root.getSpecieData().getLineage())
+            root.setLeftSpecieData(deleteSpecies(root.getLeftSpecieData(), species));
+        else if (species.getLineage() > root.getSpecieData().getLineage())
+            root.setRightSpecieData(deleteSpecies(root.getRightSpecieData(), species));
+
+        //if species is same as root's key then this is the node to be deleted
+        else{
+            //node with only one child or no child
+            if (root.getLeftSpecieData() == null)
+                return root.getRightSpecieData();
+            else if (root.getRightSpecieData() == null)
+                return root.getLeftSpecieData();
+
+            //node with two children: get the inorder successor (smalles in the right subtree)
+            root.getSpecieData().setLineage(minValue(root.getRightSpecieData()));
+            root.getRightSpecieData().setRightSpecieData(deleteSpecies(root.getRightSpecieData(), root.getSpecieData()));
+        }
 
         return root;
+    }
+
+    private int minValue(PhylogeneticBSTNode root){
+        int min = root.getSpecieData().getLineage();
+        while (root.getLeftSpecieData() != null){
+            min = root.getLeftSpecieData().getSpecieData().getLineage();
+            root = root.getLeftSpecieData();
+        }
+        return min;
     }
 
     @Override
     public PhylogeneticBSTNode searchSpecies(PhylogeneticBSTNode root, Species species) {
         // implement tree search here and modify return value
+        //Base cases: root is null or species is present at root
+        if (root == null || root.getSpecieData() == species)
+            return root;
 
-        return null;
+        //if species is greater than root's species
+        if (root.getSpecieData().getLineage() < species.getLineage())
+            return searchSpecies(root.getRightSpecieData(), species);
+        //key is smaller than root's key
+        return searchSpecies(root.getLeftSpecieData(), species);
     }
 
     @Override
     public void traversePhylogeneticBSTInorder(PhylogeneticBSTNode root) {
-        // implement inorder traversal here
-        // displays node recursively
+        /* Algorithm
+        * 1. Traverse the left subtree, i.e., call Inorder(left-subtree)
+        * 2. Visit the root.
+        * 3. Traverse the right subtree, i.e., call Inorder(right-subtree)
+        */
         if(root != null){
             traversePhylogeneticBSTInorder(root.getLeftSpecieData());
             displaySpeciesData(root.getSpecieData());
@@ -108,14 +134,32 @@ public class PhylogeneticBinarySearchTree implements PhylogeneticBSTADT {
 
     @Override
     public void traversePhylogeneticBSTPreorder(PhylogeneticBSTNode root) {
-        // implement preorder traversal here
-        // displays node recursively
+        /* Algorithm
+         * 1. Visit the root.
+         * 2.Traverse the left subtree, i.e., call Preorder(left-subtree)
+         * 3. Traverse the right subtree, i.e., call Preorder(right-subtree)
+         */
+        if (root != null){
+            displaySpeciesData(root.getSpecieData());
+            traversePhylogeneticBSTPreorder(root.getLeftSpecieData());
+            traversePhylogeneticBSTPreorder(root.getRightSpecieData());
+        }
 
     }
 
     @Override
     public void traversePhylogeneticBSTPostorder(PhylogeneticBSTNode root) {
-        // implement postorder traversal here
-        // displays node recursively
+        if(root != null){
+            /* Algorithm
+             * 1. Traverse the left subtree, i.e., call Postorder(left-subtree)
+             * 2. Traverse the right subtree, i.e., call Postorder(right-subtree)
+             * 3. Visit the root.
+             */
+
+            traversePhylogeneticBSTPostorder(root.getLeftSpecieData());
+            traversePhylogeneticBSTPostorder(root.getRightSpecieData());
+            displaySpeciesData(root.getSpecieData());
+        }
+
     }
 }
